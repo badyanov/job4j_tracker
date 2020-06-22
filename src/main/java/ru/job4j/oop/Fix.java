752e066e0b7234ca958975aa2713ca55dabe1fe4
badyanov <d.badyanov@outlook.com>
2020-06-22 08:45:18 +0300

    1.1.1. Конструктор по умолчанию. [#285770]

---
+++ src/main/java/ru/job4j/oop/Fix.java
@@ -0,0 +1,11 @@
+package ru.job4j.oop;
+
+/**
+ * 1.1.1. Конструктор по умолчанию. [#285770]
+ * @author d.badyanov@outlook.com
+ */
+public class Fix {
+    public static void main(String[] args) {
+        Fix item = new Fix();
+    }
+}
