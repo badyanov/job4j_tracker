08d28de60b764589ddb0f1b9d3da3bf5281bdb71
badyanov <d.badyanov@outlook.com>
2020-06-22 08:28:50 +0300

    1.1. Создание объекта [#285763]

---
+++ src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,17 @@
+package ru.job4j.oop;
+
+/**
+ * 1.1. Создание объекта [#285763]
+ * @author d.badyanov@outlook.com
+ */
+public class Dog {
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+
+        System.out.println(polkan);
+        System.out.println(sharik);
+        System.out.println(zhychka);
+    }
+}
