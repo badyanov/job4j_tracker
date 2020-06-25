cf92a678f9d76bb88f3d2c61be48fdd13ca5a858
badyanov <d.badyanov@outlook.com>
2020-06-25 10:02:35 +0300

    2. Перегрузка конструктора. [#285775]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,6 +4,33 @@ public class Item {
     private int id;
     private String name;
 
+    /**
+     * 2. Перегрузка конструктора. [#285775] - конструктор без параметров
+     */
+    public Item() {
+        this.id = 0;
+        this.name = "<default name>";
+    }
+
+    /**
+     * 2. Перегрузка конструктора. [#285775] - конструктор с одним параметром
+     * @param id - идентификатор
+     */
+    public Item(int id) {
+        this.id = id;
+        this.name = "<default name>";
+    }
+
+    /**
+     * 2. Перегрузка конструктора. [#285775] - конструктор с двумя параметрами
+     * @param id - идентификатор
+     * @param name - имя
+     */
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public int getId() {
         return id;
     }
