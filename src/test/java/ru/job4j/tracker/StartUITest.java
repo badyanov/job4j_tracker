8c936c7675194e05da88ae4c43fc84254fcc7f9f
badyanov <badyanov@softbalance.ru>
2020-09-15 23:08:54 +0300

    5.1. Тестирование. Подготовка данных. [#285729]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -17,4 +17,31 @@ public class StartUITest {
         Item itemExpected = new Item(taskName);
         assertThat(itemCreated.getName(), is(itemExpected.getName()));
     }
+
+    @Test
+    public void whenReplaceItem() {
+        String replacedName = "Replaced item";
+        Tracker tracker = new Tracker();
+        Item item = new Item("Old name");
+        tracker.add(item);
+        int id = item.getId();
+        String[] answers = {String.valueOf(id), replacedName};
+        Input input = new StubInput(answers);
+        StartUI.editItem(input, tracker);
+        Item itemReplaced = tracker.findById(id);
+        assertThat(itemReplaced.getName(), is(replacedName));
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("Task which will be deleted");
+        tracker.add(item);
+        int id = item.getId();
+        String[] answers = {String.valueOf(id), "Y"};
+        Input input = new StubInput(answers);
+        StartUI.deleteItem(input, tracker);
+        Item itemDeleted = tracker.findById(id);
+        assertNull(itemDeleted);
+    }
 }
