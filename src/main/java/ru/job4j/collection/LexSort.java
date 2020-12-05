adb49484413cc95da8c559a6ac9f0f2178be89dd
badyanov <d.badyanov@outlook.com>
2020-12-05 20:45:26 +0300

    4. Сортировка номера [#285694]

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -4,10 +4,38 @@ import java.util.Comparator;
 
 /**
  * 4. Сортировка номера [#285694]
+ * Компаратор для естественной сортировки названий задач в формате "%d. %s"
  */
 public class LexSort implements Comparator<String> {
     @Override
-    public int compare(String o1, String o2) {
-        return 0;
+    public int compare(String s1, String s2) {
+        String regex = "\\. ";
+        String[] ar1 = s1.split(regex, 2);
+        String[] ar2 = s2.split(regex, 2);
+
+        int length = Math.min(ar1.length, ar2.length);
+        int result;
+
+        for (int i = 0; i < length; i++) {
+            if (i == 0) {
+                result = Integer.compare(strToInt(ar1[i]), strToInt(ar2[i]));
+            } else {
+                result = ar1[i].compareTo(ar2[i]);
+            }
+            if (result != 0) {
+                return result;
+            }
+        }
+        return Integer.compare(ar1.length, ar2.length);
+    }
+
+    private int strToInt(String s) {
+        int result = 0;
+        try {
+            result = Integer.parseInt(s);
+        } catch (NumberFormatException ignored) {
+            // ignored
+        }
+        return result;
     }
 }
--- src/test/java/ru/job4j/collection/LexSortTest.java
+++ src/test/java/ru/job4j/collection/LexSortTest.java
@@ -11,5 +11,27 @@ import static org.junit.Assert.*;
  * 4. Сортировка номера [#285694]
  */
 public class LexSortTest {
-
+    @Test
+    public void sortNum1and2and10() {
+        String[] input = {
+                "10. Task.",
+                "1. Task.",
+                "1. First Task.",
+                "21. Task. Addon",
+                "21. Task.",
+                "2. Task.",
+                "11. Task."
+        };
+        String[] out = {
+                "1. First Task.",
+                "1. Task.",
+                "2. Task.",
+                "10. Task.",
+                "11. Task.",
+                "21. Task.",
+                "21. Task. Addon"
+        };
+        Arrays.sort(input, new LexSort());
+        assertThat(input, is(out));
+    }
 }
