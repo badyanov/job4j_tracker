2d25ee55c7f020b0196b7437a0f5b459eaa65a7c
badyanov <d.badyanov@outlook.com>
2020-12-09 01:18:12 +0300

    0.1. Отладка лямбда в IDEA [#285569]

--- src/main/java/ru/job4j/lambda/StreamUsage.java
+++ src/main/java/ru/job4j/lambda/StreamUsage.java
@@ -6,16 +6,37 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * 0. Stream API [#285562]
+ * 0.1. Отладка лямбда в IDEA [#285569]
  */
 public class StreamUsage {
-    public static void main(String[] args) {
-        List<Integer> numbers = new ArrayList<>(Arrays.asList(-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5));
-        System.out.println(numbers);
+    public static class Task {
+        private final String name;
+        private final long spent;
 
-        List<Integer> filtered = numbers.stream()
-                .filter(n -> n > 0)
-                .collect(Collectors.toList());
-        System.out.println(filtered);
+        public Task(String name, long spent) {
+            this.name = name;
+            this.spent = spent;
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Task> tasks = List.of(
+                new Task("Bug #1", 10),
+                new Task("Task #2", 20),
+                new Task("Bug #3", 40)
+        );
+
+        // 1. Произведите рефакторинг кода по принципу строка занимает 80 символов.
+        tasks.stream()
+                .filter(task -> task.name.contains("Bug"))
+                .map(task -> task.name)
+                .forEach(System.out::println);
+
+        // 2. Проанализируйте следующий код. Найдите в нем ошибку. Программа должна выводить задачи с scope > 30.
+        tasks.stream()
+                .filter(task -> task.name.contains("Bug"))
+                .filter(task -> task.spent > 30)
+                .map(task -> task.name + " (" + task.spent + ")")
+                .forEach(System.out::println);
     }
 }
