6cb16b382db60179406d3cda0b004a50fddd853f
badyanov <d.badyanov@outlook.com>
2020-12-09 00:05:38 +0300

    6. Функции высшего порядка [#285550]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,9 +1,11 @@
 package ru.job4j.search;
 
 import java.util.ArrayList;
+import java.util.function.Predicate;
 
 /**
  * 1. Телефонный справочник на базе ArrayList [#285699]
+ * 6. Функции высшего порядка [#285550]
  */
 public class PhoneDictionary {
     private ArrayList<Person> persons = new ArrayList<>();
@@ -13,12 +15,13 @@ public class PhoneDictionary {
     }
 
     public ArrayList<Person> find(String key) {
+        Predicate<Person> combine = (t) -> t.getName().contains(key)
+                                       || t.getSurname().contains(key)
+                                       || t.getPhone().contains(key)
+                                       || t.getAddress().contains(key);
         ArrayList<Person> result = new ArrayList<>();
         for (Person person : persons) {
-            if (person.getName().contains(key)
-                    || person.getSurname().contains(key)
-                    || person.getPhone().contains(key)
-                    || person.getAddress().contains(key)) {
+            if (combine.test(person)) {
                 result.add(person);
             }
         }
