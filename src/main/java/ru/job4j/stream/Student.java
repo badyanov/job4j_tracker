c6244e95238323a2e8b7be03f088affefa91d6d1
badyanov <d.badyanov@outlook.com>
2021-02-22 22:28:16 +0300

    1. Фильтрация учеников. [#285563]

---
+++ src/main/java/ru/job4j/stream/Student.java
@@ -0,0 +1,47 @@
+package ru.job4j.stream;
+
+import java.util.Objects;
+
+/**
+ * 1. Фильтрация учеников. [#285563]
+ */
+public class Student {
+    private int score;
+    private String surname;
+
+    public Student(int score, String surname) {
+        this.score = score;
+        this.surname = surname;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Student student = (Student) o;
+        return score == student.score
+               && Objects.equals(surname, student.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(score, surname);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s (%d)", surname, score);
+    }
+}
