38a4e686e07f73d3478162f0f7465af4c72f5907
badyanov <d.badyanov@outlook.com>
2021-02-23 23:00:36 +0300

    4. Преобразование List в Map. [#285566]

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -1,16 +1,26 @@
 package ru.job4j.stream;
 
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 /**
- * 1. Фильтрация учеников. [#285563]
+ * 4. Преобразование List в Map. [#285566]
  */
 public class School {
     public List<Student> collect(List<Student> students, Predicate<Student> predict) {
         return students.stream()
                 .filter(predict)
                 .collect(Collectors.toList());
+    }
+
+    public Map<String, Student> map(List<Student> students) {
+        return students.stream()
+                .distinct()
+                .collect(Collectors.toMap(
+                        s -> s.getSurname(),
+                        s -> s
+                ));
     }
 }
