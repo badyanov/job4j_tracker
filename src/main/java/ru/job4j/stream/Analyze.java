7a6399e5446de93e04dbcb202f278f20c68baf8a
badyanov <d.badyanov@outlook.com>
2021-03-15 00:12:04 +0300

    Крестики-нолики на JavaFX [#285578] - добавлен mapFactory для упорядочивания элементов на выходе метода averageScoreByPupil

--- src/main/java/ru/job4j/stream/Analyze.java
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -1,6 +1,7 @@
 package ru.job4j.stream;
 
 import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -19,14 +20,19 @@ public class Analyze {
 
     public static List<Tuple> averageScoreBySubject(Stream<Pupil> stream) {
         return stream.map(pupil -> new Tuple(pupil.getName(),
-                        pupil.getSubjects().stream().mapToInt(subj -> subj.getScore()).average().orElse(0D)))
+                pupil.getSubjects().stream().mapToInt(subj -> subj.getScore()).average().orElse(0D)))
                 .collect(Collectors.toList());
     }
 
     public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
         return stream.flatMap(pupil -> pupil.getSubjects().stream())
-                .collect(Collectors.groupingBy(subject -> subject.getName(),
-                        Collectors.averagingDouble(subject -> subject.getScore())))
+                .collect(
+                        Collectors.groupingBy(
+                                subject -> subject.getName(),
+                                () -> new LinkedHashMap<>(),
+                                Collectors.averagingDouble(subject -> subject.getScore())
+                                )
+                        )
                 .entrySet()
                 .stream()
                 .map(entry -> new Tuple(entry.getKey(), entry.getValue()))
@@ -35,15 +41,19 @@ public class Analyze {
 
     public static Tuple bestStudent(Stream<Pupil> stream) {
         return stream.map(pupil -> new Tuple(pupil.getName(),
-                    pupil.getSubjects().stream().mapToInt(subj -> subj.getScore()).sum()))
+                pupil.getSubjects().stream().mapToInt(subj -> subj.getScore()).sum()))
                 .max((t1, t2) -> Double.compare(t1.getScore(), t2.getScore()))
                 .orElse(null);
     }
 
     public static Tuple bestSubject(Stream<Pupil> stream) {
         return stream.flatMap(pupil -> pupil.getSubjects().stream())
-                .collect(Collectors.groupingBy(subject -> subject.getName(),
-                        Collectors.summingDouble(subject -> subject.getScore())))
+                .collect(
+                        Collectors.groupingBy(
+                                subject -> subject.getName(),
+                                Collectors.summingDouble(subject -> subject.getScore())
+                                )
+                )
                 .entrySet()
                 .stream()
                 .map(entry -> new Tuple(entry.getKey(), entry.getValue()))
